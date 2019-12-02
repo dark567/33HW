6ff2a150a222cf42f0c00fc188e39181d7832e25
dark567 <darkmit@list.ru>
2019-12-02 17:41:06 +0200

    20191202_1740

---
+++ WrongI/Action.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrongI
+{
+    class Action : IAllAction
+    {
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Movie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swim()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WrongI/IAllAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrongI
+{
+    interface IAllAction
+    {
+        void Run();
+        void Swim();
+        void Movie();
+        void Fly();
+        void Read();
+        void Write();
+
+    }
+}
